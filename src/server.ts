43f64b120ef5465c31ac831fa3520dbfedd6a44a
AugustoMachado01 <augustomachadochieno01@gmail.com>
2024-12-23 10:43:22 -0500

    add order, genre, language

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,19 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.get("/movies", async (req, res) => {
-  const movies = await prisma.movie.findMany();
+  const movies = await prisma.movie.findMany({
+    orderBy: { title: "asc" },
+    include: {
+      Genre: true,
+      Language: true,
+    },
+  });
 
   res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+  await prisma.movie.create({ data: { title: "filme de teste", genre_id: 1 } });
 });
 
 app.listen(port, () => {
