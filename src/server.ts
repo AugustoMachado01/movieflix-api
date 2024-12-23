477e9149d16d028662e8ec798eece90e721a8f1d
AugustoMachado01 <augustomachadochieno01@gmail.com>
2024-12-23 11:42:19 -0500

    add message of the error

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/movies", async (req, res) => {
   const movies = await prisma.movie.findMany({
     orderBy: { title: "asc" },
@@ -18,7 +20,38 @@ app.get("/movies", async (req, res) => {
 });
 
 app.post("/movies", async (req, res) => {
-  await prisma.movie.create({ data: { title: "filme de teste", genre_id: 1 } });
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+  if (typeof title === "number" || title.lenght === 0) {
+    return res.status(500).send("Titulo não pode ser  um numero ou vazio");
+  }
+
+  if (typeof genre_id === "string" || genre_id.lenght) {
+    return res.status(500).send("Genero não pode ser um texto ou vazio");
+  }
+
+  if (typeof language_id === "string" || genre_id.lenght) {
+    return res.status(500).send("lingua não pode ser vazio");
+  }
+
+  if (typeof genre_id === "string" || genre_id.lenght) {
+    return res.status(500).send("Genero não pode ser um texto ou vazio");
+  }
+
+  try {
+    await prisma.movie.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        release_date: new Date(release_date),
+      },
+    });
+  } catch (error: unknown) {
+    return res.status(500).json({ message: "Falha ao cadastrar um filme" });
+  }
+  res.status(201).send("criado com sucesso");
 });
 
 app.listen(port, () => {
